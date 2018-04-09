9e258006446655a3c5bae41add52d59ba34e9137
Sarah Wilson CS2015 <sarah.l.wilson.2015@uni.strath.ac.uk>
2018-04-09 21:50:05 +0100

    Upload New File

---
+++ Logout.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="CSS/LoginPage.css"/>
+    <link rel="icon" sizes="192x192" href="Images/icon.png"/>
+    <link rel="apple-touch-icon" href="Images/icon.png"/>
+    <link rel="shortcut icon" href="Images/icon.png" type="image/x-icon"/>
+
+</head>
+
+<body>
+
+You have been logged out.
+
+<a href="LoginPage.php">Log in</a>
+
+</body>
+
+</html>
