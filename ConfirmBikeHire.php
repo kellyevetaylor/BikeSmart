811053c2daae7d5f3c071b17143c2e10629ac0eb
Kelly Taylor CS2015 <kelly.taylor.2015@uni.strath.ac.uk>
2018-04-12 12:07:27 +0100

    Minor bug fixes
    Signed-off-by: Kelly Taylor CS2015 <kelly.taylor.2015@uni.strath.ac.uk>

--- ConfirmBikeHire.php
+++ ConfirmBikeHire.php
@@ -74,7 +74,7 @@ $action = isset($_POST["action"]);
         $sql = "UPDATE `BikeHubs` SET `available` = `available`+1 WHERE `id` = '$hub'";
         $conn->multi_query($sql);
 
-        $sql = "UPDATE `Bikes` SET `user` = 0 WHERE `bike` = '$bike'";
+        $sql = "UPDATE `Bikes` SET `user` = 0 WHERE `bike` = '$bike' AND `hub` = '$hub'";
         $conn->multi_query($sql);
 
         $sql = "UPDATE `Accounts` SET `hiring` = 0 WHERE `id` = '$userID'";
@@ -299,6 +299,7 @@ $action = isset($_POST["action"]);
                         }
                     ]
                 }
+            });
             });
         },
 
